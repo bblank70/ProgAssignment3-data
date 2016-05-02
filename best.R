178f595e6410886f5d4a08a9bbe752bc19c39aaa
Brian Blank <beblank@gmail.com>
2016-05-02 16:27:52 -0500

    final

--- best.R
+++ best.R
@@ -1,7 +1,22 @@
+
+
 best<-function(state, outcome) {
         ##read outcome data (outcome-of-care-measures.csv)
         ##Check state and outcome for validity
         ##Return Hospital name with lowest 30-day death rate
-        hosp<-read.csv("C:/Users/bblank/Documents/ProgAssignment3-data/outcome-of-care-measures.csv")
-        subset(hosp, state)
+        index <- ifelse(outcome == "heart attack", 11, ifelse(outcome == "heart failure", 17, 23))
+        
+        #Read and coerce our dataset while suppressing warnings and removing NA's.
+        
+        #Invalid state input or no observations
+        if (!state %in% names(states)) { 
+                stop("invalid state")
+        }
+        
+        #Slice our data by the given state and sort it by outcome and hospital name.
+        slice <- slice[order(slice[,index], na.last=TRUE),2]
+        slice <- na.omit(slice)
+        
+        #Get hospital name with the lowest 30-day mortality rate.
+        slice[1]
 }
